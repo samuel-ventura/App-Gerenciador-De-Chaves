ad6120980abc6053c6eae1ca8512bd91fe49a731
Rafael Perazzo <rafaelperazzo@gmail.com>
2021-09-27 09:29:33 -0300

    Base da aplicacao

---
+++ flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from waitress import serve
+from flask import render_template
+from flask import request,url_for,send_from_directory,redirect,flash,session
+from flask_bootstrap import Bootstrap
+from flask_wtf.csrf import CSRFProtect
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+CSRFProtect(app)
+
+@app.route('/')
+def root():
+    return ("Ola mundo!")
+
+if __name__ == "__main__":
+    serve(app, host='0.0.0.0', port=80, url_prefix='/docs')
