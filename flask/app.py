72e0da26358f884c74b5a1f83b433a328673b291
Rafael Perazzo <rafaelperazzo@gmail.com>
2021-10-13 13:39:38 -0300

    Incluido logger

--- flask/app.py
+++ flask/app.py
@@ -4,13 +4,18 @@ from flask import render_template
 from flask import request,url_for,send_from_directory,redirect,flash,session
 from flask_bootstrap import Bootstrap
 from flask_wtf.csrf import CSRFProtect
+import logging
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 CSRFProtect(app)
+CSV_DIR = '/flask/'
+
+logging.basicConfig(filename=CSV_DIR + 'app.log', filemode='w', format='%(asctime)s %(name)s - %(levelname)s - %(message)s',level=logging.DEBUG)
 
 @app.route('/')
 def root():
+    app.logger.debug('Acesso a /')
     return ("Ola mundo!")
 
 if __name__ == "__main__":
