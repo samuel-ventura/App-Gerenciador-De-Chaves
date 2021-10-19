324b66b10700be91bbca2074ddca74d2beb6528b
Rafael Perazzo <rafaelperazzo@gmail.com>
2021-10-19 14:17:41 -0300

    Para aula da SEMANA 04 - ROTAS

--- flask/app.py
+++ flask/app.py
@@ -17,5 +17,34 @@ logging.basicConfig(filename=CSV_DIR + 'app.log', filemode='w', format='%(asctim
 def root():
     return (render_template('index.html'))
 
+@app.route('/chave/cadastrar')
+def cadastrar_chave():
+    return ("Nao implementado")
+
+@app.route('/chave/listar')
+def listar_chaves():
+    return ("Nao implementado")
+
+@app.route('/usuario/listar')
+def listar_usuarios():
+    return ("Nao implementado")
+
+@app.route('/usuario/cadastrar')
+def cadastrar_usuario():
+    return ("Nao implementado")
+
+@app.route('/chave/emprestar')
+def emprestar_chave():
+    return ("Nao implementado")
+
+@app.route('/chave/listar_emprestimos')
+def listar_emprestimos():
+    return ("Nao implementado")
+
+@app.route('/chave/devolver')
+def devolver_chave():
+    return ("Nao implementado")
+
+
 if __name__ == "__main__":
     serve(app, host='0.0.0.0', port=80, url_prefix='/app')
